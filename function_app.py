a4df63b4bed224cc725ad12e3d835fc63de64632
spdtf <102963495+spdtf@users.noreply.github.com>
2025-05-09 01:03:11 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -3,8 +3,8 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="http_trigger1")
+def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -20,6 +20,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully SUHAS. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully SUHAS PD. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
